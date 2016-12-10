5c64ae8f3b7e6b63b8dc9134ba86f57c4e56cab6
OOnnnOO <hi@lidong.me>
2016-12-11 06:55:18 +0800

    Move all website files to the wiki folder,
    In order to deploy it in the subdirectory of the site

--- start.sh
+++ start.sh
@@ -3,8 +3,7 @@
 set -e
 
 chown -R nobody /var/www
-chown -R nobody /var/dokuwiki-storage
 
-su -s /bin/sh nobody -c 'php7 /var/www/bin/indexer.php -c'
+su -s /bin/sh nobody -c 'php7 /var/www/wiki/bin/indexer.php -c'
 
 exec /usr/bin/supervisord -c /etc/supervisord.conf
